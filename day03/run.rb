7b86edc8fc4f11d3678d91de036a56efd112ef61
Andy Luo <aluo@greatschools.org>
2020-12-03 08:55:46 -0800

    day 03

---
+++ day03/run.rb
@@ -0,0 +1,145 @@
+# --- Day 3: Toboggan Trajectory ---
+# With the toboggan login problems resolved, you set off toward 
+# the airport. While travel by toboggan might be easy, it's certainly
+#  not safe: there's very minimal steering and the area is covered 
+# in trees. You'll need to see which angles will take you near 
+# the fewest trees.
+
+# Due to the local geology, trees in this area only grow on exact 
+# integer coordinates in a grid. You make a map (your puzzle input) 
+# of the open squares (.) and trees (#) you can see. For example:
+
+# ..##.......
+# #...#...#..
+# .#....#..#.
+# ..#.#...#.#
+# .#...##..#.
+# ..#.##.....
+# .#.#.#....#
+# .#........#
+# #.##...#...
+# #...##....#
+# .#..#...#.#
+# These aren't the only trees, though; due to something you read 
+# about once involving arboreal genetics and biome stability, the 
+# same pattern repeats to the right many times:
+
+# ..##.........##.........##.........##.........##.........##.......  --->
+# #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+# .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+# ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+# .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+# ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+# .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+# .#........#.#........#.#........#.#........#.#........#.#........#
+# #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+# #...##....##...##....##...##....##...##....##...##....##...##....#
+# .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+# You start on the open square (.) in the top-left corner and need 
+# to reach the bottom (below the bottom-most row on your map).
+
+# The toboggan can only follow a few specific slopes (you opted for a 
+# cheaper model that prefers rational numbers); start by counting
+#  all the trees you would encounter for the slope right 3, down 1:
+
+# From your starting position at the top-left, check the position 
+# that is right 3 and down 1. Then, check the position that is right 
+# 3 and down 1 from there, and so on until you go past the bottom of 
+# the map.
+
+# The locations you'd check in the above example are marked here 
+# with O where there was an open square and X where there was a tree:
+
+# ..##.........##.........##.........##.........##.........##.......  --->
+# #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+# .#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+# ..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+# .#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+# ..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+# .#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+# .#........#.#........X.#........#.#........#.#........#.#........#
+# #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+# #...##....##...##....##...#X....##...##....##...##....##...##....#
+# .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+# In this example, traversing the map using this slope would cause 
+# you to encounter 7 trees.
+
+# Starting at the top-left corner of your map and following a 
+# slope of right 3 and down 1, how many trees would you encounter?
+
+class Map
+  attr_accessor :map, :count, :current_col, :current_row
+
+  def initialize
+    @map = Map.create_map
+    @count = 0
+    @current_col = 0
+    @current_row = 0
+  end
+
+  def self.create_map
+    file = File.read("day03/input.txt")
+    rows = file.split(/\n/)
+    map = Array.new(rows.length) { Array.new(rows.first.length) }
+    rows.each_with_index do |row, row_idx|
+      row.chars.each_with_index do |terrain, col_idx|
+        map[row_idx][col_idx] = terrain
+      end
+    end
+  end
+
+  def mark_map(row_idx, col_idx)
+    # wraps the columns if needed
+    @current_col = @current_col % @map.first.length
+    @count +=1 if @map[@current_row][@current_col] == '#'
+  end
+
+  def run(slope_col, slope_row)
+    while @current_col <= @map.length
+      # puts "Current Row is: #{@current_row}"
+      # puts "Current Col is: #{@current_col}"
+      @current_row += slope_row
+      @current_col += slope_col
+      break if @current_row >= @map.length
+      mark_map(@current_row, @current_col)
+    end
+    @count
+  end
+end
+
+# Part A
+map = Map.new
+slope_1 = map.run(3,1)
+puts slope_1
+
+# --- Part Two ---
+# Time to check the rest of the slopes - you need to minimize 
+# the probability of a sudden arboreal stop, after all.
+
+# Determine the number of trees you would encounter if, for each 
+# of the following slopes, you start at the top-left corner and 
+# traverse the map all the way to the bottom:
+
+# Right 1, down 1.
+# Right 3, down 1. (This is the slope you already checked.)
+# Right 5, down 1.
+# Right 7, down 1.
+# Right 1, down 2.
+# In the above example, these slopes would find 2, 7, 3, 4, and 2 
+# tree(s) respectively; multiplied together, these produce the 
+# answer 336.
+
+# What do you get if you multiply together the number of trees 
+# encountered on each of the listed slopes?
+
+# Part B
+slope_2 = Map.new.run(1,1)
+slope_3 = Map.new.run(5,1)
+slope_4 = Map.new.run(7,1)
+slope_5 = Map.new.run(1,2)
+
+puts slope_1 * slope_2 * slope_3 * slope_4 * slope_5
+
+
+
+
